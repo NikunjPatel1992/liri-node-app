67fb35c72191ebb2f2537636a6133f5c034e7309
nikunj <nikupatel199@gmail.com>
2018-12-11 17:59:59 -0600

    write file

--- liri.js
+++ liri.js
@@ -9,37 +9,130 @@ var Spotify = require('node-spotify-api');
 
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
-var song ="";
+var text = "";
 
 var fs = require("fs");
 
+// =====================================
+// ========= inquirer=====================
 var inquirer = require("inquirer");
 
-switch (action) {
-  case "concert-this":
-    artist();
-    break;
+// Create a "Prompt" with a series of questions.
+inquirer
+  .prompt([
+    // Here we create a basic text prompt.
+    {
+      type: "input",
+      message: "What is your name?",
+      name: "username"
+    },
+    // Here we create a basic password-protected text prompt.
+    {
+      type: "password",
+      message: "Set your password",
+      name: "password"
+    },
+    // Here we give the user a list to choose from.
+    {
+      type: "list",
+      message: "Select for run?",
+      choices: ["concert-this", "spotify-this-song", "movie-this", "do-what-it-says"],
+      name: "select"
+    },
+    // Here we ask the user to confirm.
+    // {
+    //   type: "confirm",
+    //   message: "Are you sure:",
+    //   name: "confirm",
+    //   default: true
+    // }
+  ])
+  .then(function (inquirerResponse) {
+    logData("\n" + inquirerResponse.username);
+    switch (inquirerResponse.select) {
+      case "concert-this":
+        inquirer
+          .prompt([
+            {
+              type: "input",
+              message: "Type concert name",
+              name: "concertname"
+            }
+          ])
+          .then(function (concert) {
+            logData(" concert-this :" + concert.concertname);
+            text = concert.concertname;
+            artist(text);
+          });
+        break;
 
-  case "spotify-this-song":
-    song = userinput;
-    spotifysong(song);
-    break;
+      case "spotify-this-song":
+        inquirer
+          .prompt([
+            {
+              type: "input",
+              message: "Type song name",
+              name: "songname"
+            }
+          ])
+          .then(function (play) {
+            logData(" spotify-this-song :" + play.songname);
+            text = play.songname;
+            spotifysong(text);
+          });
+        break;
 
-  case "movie-this":
-    movie();
-    break;
+      case "movie-this":
+        inquirer
+          .prompt([
+            {
+              type: "input",
+              message: "Type movie name",
+              name: "moviename"
+            }
+          ])
+          .then(function (play) {
+            logData(" movie-this :" + play.moviename);
+            text = play.moviename;
+            movie(text);
+          });
+        break;
 
-  case "do-what-it-says":
-    dowhatitsays();
-    break;
+      case "do-what-it-says":
+        logData(" do-what-it-says");
+        dowhatitsays();
+        break;
+    }
+  })
 
-}
+// =====================================
+// ========= inquirer=====================
 
-function artist() {
-  axios.get("https://rest.bandsintown.com/artists/" + userinput + "/events?app_id=codingbootcamp").then(
+// switch (action) {
+//   case "concert-this":
+//     artist();
+//     break;
+
+//   case "spotify-this-song":
+//     song = userinput;
+//     spotifysong(song);
+//     break;
+
+//   case "movie-this":
+//     movie();
+//     break;
+
+//   case "do-what-it-says":
+//     dowhatitsays();
+//     break;
+
+// }
+
+function artist(concert) {
+  axios.get("https://rest.bandsintown.com/artists/" + concert + "/events?app_id=codingbootcamp").then(
     function (response) {
       console.log("key loaded");
-      console.log("Upcoming concerts for " + userinput + " :");
+      console.log("Upcoming concerts for " + concert + " :");
 
       for (var i = 0; i < response.data.length; i++) {
         var datetime = response.data[i].datetime;
@@ -53,8 +146,8 @@ function artist() {
     });
 }
 // ==========movie this ====================
-function movie() {
-  axios.get("http://www.omdbapi.com/?t=" + userinput + "&y=&plot=short&apikey=trilogy").then(
+function movie(movie) {
+  axios.get("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy").then(
     function (response) {
       console.log("key loaded");
       console.log("Title : " + response.data.Title);
@@ -93,8 +186,8 @@ function spotifysong(song) {
 // ==========spotify this song ====================
 
 // ============do-what-it-says==================
-function dowhatitsays(){
-  fs.readFile("random.txt", "utf8", function(error, data) {
+function dowhatitsays() {
+  fs.readFile("random.txt", "utf8", function (error, data) {
 
     if (error) {
       return console.log(error);
@@ -104,6 +197,21 @@ function dowhatitsays(){
     var songname = data.split('"')
     // console.log(songname[1]);
     spotifysong(songname[1]);
-  });  
+  });
 }
 // ============do-what-it-says==================
+
+function logData(userdata) {
+
+  fs.appendFile("log.txt", userdata, function (err) {
+    // If an error was experienced we will log it.
+    if (err) {
+      console.log(err);
+    }
+    // If no error is experienced, we'll log the phrase "Content Added" to our node console.
+    else {
+      // console.log("Content Added!");
+    }
+
+  });
+}
