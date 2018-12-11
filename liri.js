ddc389b8135a90391243e1069a26e2f6110355f0
nikunj <nikupatel199@gmail.com>
2018-12-11 16:23:38 -0600

    file read

--- liri.js
+++ liri.js
@@ -4,14 +4,16 @@ var userinput = process.argv[3];
 var action = process.argv[2];
 
 var axios = require("axios");
-var spotify = require('spotify');
 var Spotify = require('node-spotify-api');
 
-// require("dotenv").config();
-var keys = require("./keys.js");
-var spo = new Spotify(keys.spotify);
-// var key = require("./keys.js")
 
+var keys = require("./keys.js");
+var spotify = new Spotify(keys.spotify);
+var song ="";
+
+var fs = require("fs");
+
+var inquirer = require("inquirer");
 
 switch (action) {
   case "concert-this":
@@ -19,7 +21,8 @@ switch (action) {
     break;
 
   case "spotify-this-song":
-    song();
+    song = userinput;
+    spotifysong(song);
     break;
 
   case "movie-this":
@@ -27,7 +30,7 @@ switch (action) {
     break;
 
   case "do-what-it-says":
-
+    dowhatitsays();
     break;
 
 }
@@ -37,12 +40,12 @@ function artist() {
     function (response) {
       console.log("key loaded");
       console.log("Upcoming concerts for " + userinput + " :");
-      
+
       for (var i = 0; i < response.data.length; i++) {
         var datetime = response.data[i].datetime;
-        var dt= [] = datetime.split("T");
+        var dt = [] = datetime.split("T");
         var date = [] = dt[0].split("-");
-        console.log(response.data[i].venue.city + " , " + response.data[i].venue.country + " at " + response.data[i].venue.name+" "+date[2]+"-"+date[1]+"-"+date[0]);
+        console.log(response.data[i].venue.city + " , " + response.data[i].venue.country + " at " + response.data[i].venue.name + " " + date[2] + "-" + date[1] + "-" + date[0]);
         // console.log(response.data[i].venue);
         // console.log(response.data[i].datetime);
       }
@@ -68,28 +71,39 @@ function movie() {
 // ==========movie this ====================
 
 // ==========spotify this song ====================
-function song() {
-  var spotify = new Spotify({
-    id: "f0702defba8e47259a974403ad056a20",
-    secret: "346187bbbbc8448aa7e6c90660c18b85"
-  });
+function spotifysong(song) {
 
-  spo.search({ type: 'track', query: 'celebration', limit: 20 }, function (err, data) {
+  spotify.search({ type: 'track', query: song, limit: 20 }, function (err, data) {
     if (err) {
       return console.log('Error occurred: ' + error);
     }
-    for (var i = 0 ; i < data.tracks.items.length; i++){
+    for (var i = 0; i < data.tracks.items.length; i++) {
       console.log(i);
-      console.log("artist(s) : "+data.tracks.items[i].album.artists[0].name);
-      console.log("song name : "+data.tracks.items[i].name);
-      console.log("preview song : "+data.tracks.items[i].preview_url)
-      console.log("album : "+data.tracks.items[i].album.name)
+      console.log("artist(s) : " + data.tracks.items[i].album.artists[0].name);
+      console.log("song name : " + data.tracks.items[i].name);
+      console.log("preview song : " + data.tracks.items[i].preview_url)
+      console.log("album : " + data.tracks.items[i].album.name)
       console.log("----------------------------------------------")
     }
     // console.log(data.tracks.items)
-    
+
   });
 
 }
-
 // ==========spotify this song ====================
+
+// ============do-what-it-says==================
+function dowhatitsays(){
+  fs.readFile("random.txt", "utf8", function(error, data) {
+
+    if (error) {
+      return console.log(error);
+    }
+
+    // console.log(data);
+    var songname = data.split('"')
+    // console.log(songname[1]);
+    spotifysong(songname[1]);
+  });  
+}
+// ============do-what-it-says==================
